18009d1b26b1f8b9d968f47dce25cfe8a7d1a483
Bilalmc <zuversenden@gmx.de>
2026-09-06 17:34:48 +0200

    Fix root Gradle plugin aliases

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import com.android.build.api.dsl.ApplicationExtension
 
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.android.test) apply false
