13f9c438c98dc3b12992e08811ea3a78c37d7200
Bilalmc <zuversenden@gmx.de>
2026-09-10 13:10:59 +0200

    Raise product UI compileSdk to Android 37

--- product-ui/build.gradle.kts
+++ product-ui/build.gradle.kts
@@ -8,11 +8,7 @@ plugins {
 
 android {
     namespace = "com.bilalmc.iptvplayer.ui"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 26
